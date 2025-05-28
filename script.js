59880264754cac51a8f1a22c2d753c1893137317
ThamilyBravo <thamilyrsbravo17@gmail.com>
2025-05-28 12:05:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("myButton").addEventListener("click", function() {
-  alert("Você clicou no botão!");
+  alert("1, você é meu biju; 2, tenho dependência emocional em você; 3, seu beijo tem bafo; 4, você fede; 5,pefiro ser surdo do que ouvir sua voz.");
 });
